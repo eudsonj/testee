d902140d639284c23e2111bd22988c952b61327d
eudsonj <silva.eudson@escola.pr.gov.br>
2026-02-24 08:18:22 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,16 @@
+console.log
+("hello word");
+console.log
+(0123456789);
+console.log
+("eudson");
+x=8-9;
+console.log(x);
+y=81;
+z=9;
+m=y+z;
+t=m+x
+console.log (m) ;
+console.log("O resultado é" ,m);
+console.log("o outro resultado é ",x)
+console.log("o resultado total é", t)
